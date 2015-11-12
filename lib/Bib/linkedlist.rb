11c58f4f46c52ebeeeb2512a629c7876c6371889
alu0100825145 <alu0100825145@ull.edu.es>
2015-11-12 22:17:53 +0000

    da error porque falta crear los metodos ext y inicio

--- lib/Bib/linkedlist.rb
+++ lib/Bib/linkedlist.rb
@@ -4,4 +4,11 @@ class Lista
    def initialize(nodo)
        @inicio = Nodostruct.new(nil)
    end
+   
+   def push(nodo)
+        if @inicio != nil
+            nodo.siguiente = @inicio
+            @inicio = nodo
+        end        
+   end
 end
