f9df91d861c5031816452e90849caca1d684414b
alu0100825145 <alu0100825145@ull.edu.es>
2015-11-19 20:21:42 +0000

    Creada expectativa para comprobar que Libros es una clase hija de Biblio | la falla

--- spec/bib_spec.rb
+++ spec/bib_spec.rb
@@ -1,7 +1,18 @@
 require "spec_helper"
 require "Bib"
 
+#Clase Padre
+
 describe Biblio do
-
-
+    before :each do
+         @libro = Libros.new("CATEDRA", "ISBN: 9780828825115")
+    end
+    
+    #Clases de la herencia
+    describe "Clases de la Herencia de Biblio" do
+        it "Libros" do
+          expect(@libro.is_a? Libros).to eq(true)
+          expect(@libro.kind_of? Biblio).to eq(true)
+        end 
+    end
 end
