3dac51a223129dc51c74ee384847ae654500cb73
alu0100825145 <alu0100825145@ull.edu.es>
2015-11-19 21:14:53 +0000

    Creada expectativa para comprobar que Articulosweb es una clase hija de Biblio | la falla

--- spec/bib_spec.rb
+++ spec/bib_spec.rb
@@ -5,8 +5,8 @@ require "Bib"
 
 describe Biblio do
     before :each do
-         @libro = Libros.new("CATEDRA", "ISBN: 9780828825115")
-        
+        @libro = Libros.new("CATEDRA", "ISBN: 9780828825115")
+        @articuloweb = Articulosweb.new("www.dejensever.com/2015/11/08/las-cuevecitas-fs-vs-cfs-chinguaro-en-imagenes/")
     end
     
     #Clases de la herencia
@@ -15,7 +15,10 @@ describe Biblio do
             expect(@libro.is_a? Libros).to eq(true)
             expect(@libro.kind_of? Biblio).to eq(true)
         end 
-
+        it "Articulos Web" do
+            expect(@articuloweb.is_a? Articulosweb).to eq(true)
+            expect(@articuloweb.kind_of? Biblio).to eq(true)
+        end
     end
     
 
