7537da15e9f698de7e58b9caf8a5af475df2d5ae
alu0100825145 <alu0100825145@ull.edu.es>
2015-11-19 19:47:54 +0000

    Creada la clase padre de la herencia con los elementos comunes

--- lib/Bib/Bib.rb
+++ lib/Bib/Bib.rb
@@ -1,19 +1,20 @@
 class Biblio
     
-    attr_reader :autores, :titulo, :serie, :editorial, :numedicion, :fecha, :isbn
+    attr_accessor :autores, :titulo, :fecha
         
-    def initialize(autores, titulo, serie = nil, editorial, numedicion, fecha, isbn)
+    def initialize(autores, titulo, fecha)
         @autores = autores
         @titulo = titulo
-        @serie = serie
-        @editorial = editorial
-        @numedicion = numedicion
         @fecha = fecha
-        @isbn = isbn
     end
     
-    def to_s()
-       "#{@autores[0]}, #{@autores[1]}, #{@autores[2]} \n #{@titulo} \n #{@serie} \n #{@editorial} \n #{@numedicion} \n #{@fecha} \n #{@isbn}"
+    def get_autores() 
+        return @autores
     end
-    
+    def get_titulo() 
+        return @titulo          
+    end
+    def get_fecha() 
+        return @fecha
+    end
 end
