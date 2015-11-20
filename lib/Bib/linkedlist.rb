ee06316077d9662c7c0a11a64eb54deda5189dc1
alu0100825145 <alu0100825145@ull.edu.es>
2015-11-20 00:56:10 +0000

    La estructura de los Nodos es correcta (Pasa la prueba). Además clase Double_List inicializada

--- lib/Bib/linkedlist.rb
+++ lib/Bib/linkedlist.rb
@@ -1,5 +1,8 @@
-
-
-class Lista
-
+Nodostruct = Struct.new(:prev, :value, :next) 
+class Double_List
+    attr_accessor :head, :tail
+    def initialize(head, tail)   
+        @head = head 
+        @tail = tail 
+    end
 end
