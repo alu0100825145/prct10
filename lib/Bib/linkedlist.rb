da0386a5c02632abd946eebc0a63b5f08dd9482a
alu0100825145 <alu0100825145@ull.edu.es>
2015-11-20 01:03:25 +0000

    Creado el metodo para comprobar que hay una lista vacia (pasa la prueba)

--- lib/Bib/linkedlist.rb
+++ lib/Bib/linkedlist.rb
@@ -1,8 +1,15 @@
 Nodostruct = Struct.new(:prev, :value, :next) 
 class Double_List
     attr_accessor :head, :tail
+    #Constructor
     def initialize(head, tail)   
         @head = head 
         @tail = tail 
     end
+    
+    #Metodo Lista vacia
+    def empty? 
+        @head == nil
+    end
+      
 end
