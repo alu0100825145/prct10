20ce0075ec8a83409dcb0e9b1a06cfe04175edef
alu0100825145 <alu0100825145@ull.edu.es>
2015-12-10 21:18:06 +0000

    Creado metodo para insertar referencias APA e inicializado el constructor de la clase|supera la prueba

--- lib/Bib/APA.rb
+++ lib/Bib/APA.rb
@@ -1,5 +1,12 @@
 require './lib/Bib/linkedlist.rb'
 class APAref 
-         
-
+    include Enumerable
+        
+    def initialize() 
+		@list = Double_List.new(nil,nil)
+    end
+		
+	def insertref(ref) 
+		@list.insert(ref)
+	end    
 end
